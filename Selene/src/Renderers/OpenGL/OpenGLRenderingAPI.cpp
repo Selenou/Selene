938c9ee14ac840719ed2d61ca6116be5c94648ca
Selenou <romain.rockmania@gmail.com>
2020-11-14 10:19:00 -0500

    opengl error callback

--- Selene/src/Renderers/OpenGL/OpenGLRenderingAPI.cpp
+++ Selene/src/Renderers/OpenGL/OpenGLRenderingAPI.cpp
@@ -4,9 +4,40 @@
 
 namespace Selene 
 {
+
+	void OpenGLMessageCallback(
+		unsigned source,
+		unsigned type,
+		unsigned id,
+		unsigned severity,
+		int length,
+		const char* message,
+		const void* userParam)
+	{
+		switch (severity)
+		{
+			case GL_DEBUG_SEVERITY_HIGH:   
+				SLN_ENGINE_CRITICAL(message); 
+				return;
+			case GL_DEBUG_SEVERITY_MEDIUM:  
+				SLN_ENGINE_ERROR(message); 
+				return;
+			case GL_DEBUG_SEVERITY_LOW:   
+				SLN_ENGINE_WARN(message); 
+				return;
+			case GL_DEBUG_SEVERITY_NOTIFICATION: 
+				SLN_ENGINE_TRACE(message); 
+				return;
+		}
+	}
+
 	void OpenGLRenderingAPI::Init()
 	{
-
+		// Debug Output
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
 	}
 
 	void OpenGLRenderingAPI::Clear()
